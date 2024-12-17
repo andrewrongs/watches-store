51823c169d5b3f043e8c745427b4ccd1c282f6d1
Andrew <fter2257@gmail.com>
2024-12-17 23:42:49 +0800

    Update

---
+++ assets/ssrBoot-86bec505.js
@@ -0,0 +1,1 @@
+import{s,A as t,n as e,aH as n}from"./index-d4e5e210.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
